3b472284ef5038ea7523b0b1618864fbf4b185a4
ligia.aluccas@gmail.com <ligia.aluccas@gmail.com>
2022-12-14 00:26:32 +0000

    Aprendendo Select*from

---
+++ dia01/select.sql
@@ -0,0 +1,14 @@
+-- Databricks notebook source
+select 'Olá mundo, aqui quem fala é da Terra'
+
+-- COMMAND ----------
+
+select 1 + 10 * 2
+
+-- COMMAND ----------
+
+select ABS(10-1000)
+
+-- COMMAND ----------
+
+
